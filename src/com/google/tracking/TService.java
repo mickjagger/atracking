bcd690589aed0f44303724a163f20b2a334aadb5
mickjagger <yanukovichm@gmail.com>
2016-03-18 12:49:12 +0300

    start sticky service, refactor call recording

--- src/com/google/tracking/TService.java
+++ src/com/google/tracking/TService.java
@@ -12,6 +12,7 @@ import android.os.IBinder;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.Toast;
+import com.google.tracking.call.RecorderController;
 
 import java.io.File;
 import java.io.IOException;
@@ -33,7 +34,7 @@ public class TService extends Service {
     Boolean offHook = false, ringing = false;
     Toast toast;
     Boolean isOffHook = false;
-    private boolean recordstarted = false;
+
 
     private static final String ACTION_IN = "android.intent.action.PHONE_STATE";
     private static final String ACTION_OUT = "android.intent.action.NEW_OUTGOING_CALL";
@@ -78,7 +79,7 @@ public class TService extends Service {
         // if(terminate != null) {
         // stopSelf();
         // }
-        return START_NOT_STICKY;
+        return START_STICKY;
     }
 
     public class CallBr extends BroadcastReceiver {
@@ -86,103 +87,50 @@ public class TService extends Service {
         String state;
         String inCall, outCall;
         public boolean wasRinging = false;
-//        private MediaRecorder recorder;
+        RecorderController recorderController;
+
+        public CallBr(){
+            recorderController = new RecorderController();
+        }
 
         @Override
         public void onReceive(Context context, Intent intent) {
+            String phoneNumber = getResultData();
+            if (phoneNumber == null) {
+                // No reformatted number, use the original
+                phoneNumber = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
+            }
+
             if (intent.getAction().equals(ACTION_IN)) {
                 if ((bundle = intent.getExtras()) != null) {
                     state = bundle.getString(TelephonyManager.EXTRA_STATE);
                     if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
                         inCall = bundle.getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
                         wasRinging = true;
-                        Toast.makeText(context, "IN : " + inCall, Toast.LENGTH_LONG).show();
+                        Toast.makeText(context, "IN : " + inCall + " " + phoneNumber, Toast.LENGTH_LONG).show();
                     } else if (state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
                         if (wasRinging == true) {
 
-                            Toast.makeText(context, "ANSWERED", Toast.LENGTH_LONG).show();
+                            Toast.makeText(context, "ANSWERED" + " " + phoneNumber, Toast.LENGTH_LONG).show();
+                            recorderController.record(phoneNumber);
 
-                            String out = new SimpleDateFormat("dd-MM-yyyy hh-mm-ss").format(new Date());
-                            File sampleDir = new File(Environment.getExternalStorageDirectory(), "/TestRecordingDasa1");
-                            if (!sampleDir.exists()) {
-                                sampleDir.mkdirs();
-                            }
-                            String file_name = "Record";
-                            try {
-                                audiofile = File.createTempFile(file_name, ".amr", sampleDir);
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                            String path = Environment.getExternalStorageDirectory().getAbsolutePath();
-
-                            recorder = new MediaRecorder();
-//                          recorder.setAudioSource(MediaRecorder.AudioSource.VOICE_CALL);
-
-                            recorder.setAudioSource(MediaRecorder.AudioSource.VOICE_COMMUNICATION);
-                            recorder.setOutputFormat(MediaRecorder.OutputFormat.AMR_NB);
-                            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-                            recorder.setOutputFile(audiofile.getAbsolutePath());
-                            try {
-                                recorder.prepare();
-                            } catch (IllegalStateException e) {
-                                e.printStackTrace();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                            recorder.start();
-                            recordstarted = true;
                         }
                     } else if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
                         wasRinging = false;
-                        Toast.makeText(context, "REJECT || DISCO", Toast.LENGTH_LONG).show();
-                        if (recordstarted) {
-                            recorder.stop();
-                            recordstarted = false;
-                        }
+                        Toast.makeText(context, "REJECT || DISCO" + " " + phoneNumber, Toast.LENGTH_LONG).show();
+                        recorderController.stop();
                     }
                 }
             } else if (intent.getAction().equals(ACTION_OUT)) {
                 if ((bundle = intent.getExtras()) != null) {
                     outCall = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
-                    Toast.makeText(context, "OUT : " + outCall, Toast.LENGTH_LONG).show();
-                    record();
+                    Toast.makeText(context, "OUT : " + outCall + " " + phoneNumber, Toast.LENGTH_LONG).show();
+                    recorderController.record(phoneNumber);
                 }
             }
         }
 
-        private void record()
-        {
-            String out = new SimpleDateFormat("dd-MM-yyyy hh-mm-ss").format(new Date());
-            File sampleDir = new File(Environment.getExternalStorageDirectory(), "/DCIM");
-            if (!sampleDir.exists()) {
-                sampleDir.mkdirs();
-            }
-            String file_name = "Record";
-            try {
-                audiofile = File.createTempFile(file_name, ".amr", sampleDir);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            String path = Environment.getExternalStorageDirectory().getAbsolutePath();
 
-            recorder = new MediaRecorder();
-//                          recorder.setAudioSource(MediaRecorder.AudioSource.VOICE_CALL);
-
-            recorder.setAudioSource(MediaRecorder.AudioSource.VOICE_COMMUNICATION);
-            recorder.setOutputFormat(MediaRecorder.OutputFormat.AMR_NB);
-            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-            recorder.setOutputFile(audiofile.getAbsolutePath());
-//            recorder.setOutputFile(Environment.getExternalStorageDirectory().getAbsolutePath() + "/DCIM/aud.amr");
-            try {
-                recorder.prepare();
-            } catch (IllegalStateException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            recorder.start();
-            recordstarted = true;
-        }
     }
 
 }
---
+++ src/com/google/tracking/call/RecorderController.java
@@ -0,0 +1,67 @@
+package com.google.tracking.call;
+
+import android.media.MediaRecorder;
+import android.os.Environment;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class RecorderController {
+    private MediaRecorder recorder;
+    private boolean recordstarted = false;
+    File audiofile;
+
+    public void stop()
+    {
+        if(recordstarted)
+        {
+            recorder.stop();
+        }
+        recordstarted = false;
+    }
+
+    public void record(String phoneNumber) {
+        if(recordstarted)
+        {
+//            throw new Error("Trying to start new record while previous not stopped");
+            stop();
+        }
+
+        File sampleDir = new File(Environment.getExternalStorageDirectory(), "/DCIM");
+        if (!sampleDir.exists()) {
+            sampleDir.mkdirs();
+        }
+
+        String time_stamp = new SimpleDateFormat("yyyy_MM_dd_hh_mm_ss").format(new Date());
+        String file_name = time_stamp + "_n_";
+        file_name += (phoneNumber != null) ? phoneNumber : "unknown_phone_number";
+
+
+        try {
+            audiofile = File.createTempFile(file_name, ".amr", sampleDir);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+        recorder = new MediaRecorder();
+//                          recorder.setAudioSource(MediaRecorder.AudioSource.VOICE_CALL);
+
+        recorder.setAudioSource(MediaRecorder.AudioSource.VOICE_COMMUNICATION);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.AMR_NB);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        recorder.setOutputFile(audiofile.getAbsolutePath());
+//            recorder.setOutputFile(Environment.getExternalStorageDirectory().getAbsolutePath() + "/DCIM/aud.amr");
+        try {
+            recorder.prepare();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        recorder.start();
+        recordstarted = true;
+    }
+}
