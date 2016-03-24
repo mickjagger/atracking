5399ff9329ad87b676d88ecf37cb9c3a3746ae3e
mickjagger <yanukovichm@gmail.com>
2016-03-24 13:18:58 +0300

    add server service sources

---
+++ webservice/receiver.php
@@ -0,0 +1,35 @@
+<?php
+date_default_timezone_set('Europe/Minsk');
+$logname = 'log.txt';
+$filename = 'received.txt';
+$date = date("d-m-Y h:i:s");
+$somecontent = "\r\n".$_REQUEST['model']." ".$date."\r\n".$_REQUEST['msg'];
+$action = $_REQUEST['action'];
+// Вначале давайте убедимся, что файл существует и доступен для записи.
+// if (is_writable($filename)) {
+
+    // В нашем примере мы открываем $filename в режиме "дописать в конец".
+    // Таким образом, смещение установлено в конец файла и
+    // наш $somecontent допишется в конец при использовании fwrite().
+    if (!$handle = fopen($filename, 'a')) {
+         echo "cant open ($filename)";
+         exit;
+    }
+
+    // Записываем $somecontent в наш открытый файл.
+    if (fwrite($handle, $somecontent) === FALSE) {
+        echo "cant write ($filename)";
+        exit;
+    }
+    
+    fclose($handle);
+
+// } else {
+    // echo "$filename not writable";
+// }
+switch($action){
+case "command":
+	echo "magic";
+break;
+}
+?>
