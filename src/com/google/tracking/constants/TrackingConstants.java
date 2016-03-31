0f95e74fc3d42ed45771b5bea30b11c7a6b8723d
mickjagger <yanukovichm@gmail.com>
2016-03-31 14:55:38 +0300

    change record voice settings

--- src/com/google/tracking/constants/TrackingConstants.java
+++ src/com/google/tracking/constants/TrackingConstants.java
@@ -1,8 +1,8 @@
 package com.google.tracking.constants;
 
 public final class TrackingConstants {
-    public static int RUN_REPEATING_TASK_INTERVAL = 1 * 1000 * 60;
+    public static int RUN_REPEATING_TASK_INTERVAL = 3 * 1000 * 60;
     public static String FILES_PATH = "/DCIM/fls";
     public static String LOCATIONS_FILE = "loc";
-    public static int MIN_AMPLITUDE_TO_RECORD_VOICE = 3500;
+    public static int MIN_AMPLITUDE_TO_RECORD_VOICE = 5500;
 }
